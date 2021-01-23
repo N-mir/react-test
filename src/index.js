86b834f82ff429193b96d8bc6999ed9d26c4c18e
N-mir <47846195+N-mir@users.noreply.github.com>
2021-01-22 21:29:35 -0900

    JSX記法

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import { StrictMode } from "react";
-import ReactDOM from "react-dom";
+import React from "react";
+import ReactDom from "react-dom";
 
-import App from "./App";
+const App = () => {
+  return (
+    // returnの中にhtmlを書く
+    // <></>の間に書く
+    // コンポーネントで分割するのが基本
+    <>
+      <h1>こんにちは！</h1>
+      <p>お元気ですか？</p>
+    </>
+  );
+};
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  rootElement
-);
+ReactDom.render(<App />, document.getElementById("root"));
