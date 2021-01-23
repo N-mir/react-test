299fa02b253d66a202b22183c5d91e0d8fdb5304
N-mir <47846195+N-mir@users.noreply.github.com>
2021-01-22 21:43:53 -0900

    コンポーネント

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () => {
+  return (
+    // returnの中にhtmlを書く
+    // <></>の間に書く
+    // コンポーネントで分割するのが基本
+    <>
+      <h1>こんにちは！</h1>
+      <p>お元気ですか？</p>
+    </>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
 import React from "react";
 import ReactDom from "react-dom";
-
-const App = () => {
-  return (
-    // returnの中にhtmlを書く
-    // <></>の間に書く
-    // コンポーネントで分割するのが基本
-    <>
-      <h1>こんにちは！</h1>
-      <p>お元気ですか？</p>
-    </>
-  );
-};
+import App from "./App";
 
 ReactDom.render(<App />, document.getElementById("root"));
